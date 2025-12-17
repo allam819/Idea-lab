c56191bc6c6b4095c94804e96698046dd69bae9a
allam <allamansari819@gmail.com>
2025-12-18 02:16:12 +0530

    final

--- client/src/components/ImageNode.jsx
+++ client/src/components/ImageNode.jsx
@@ -6,9 +6,10 @@ const ImageNode = ({ data, selected }) => {
   return (
     <div style={{ 
       position: 'relative', 
-      width: 'fit-content',  // <--- KEY FIX: Hugs the image size
-      height: 'fit-content', // <--- KEY FIX: Hugs the image size
-      lineHeight: 0          // Removes tiny ghost space at bottom
+      // width: 'fit-content' tells the box to shrink to the image size
+      width: 'fit-content',
+      height: 'fit-content',
+      lineHeight: 0 
     }}>
       
       {/* THE IMAGE */}
@@ -17,50 +18,36 @@ const ImageNode = ({ data, selected }) => {
         alt="Upload" 
         style={{ 
           display: 'block',
-          maxWidth: '300px', // Prevents giant images
-          height: 'auto',    // Maintains Aspect Ratio!
+          // Force natural height/width ratio
+          width: 'auto',   
+          height: 'auto',  
+          maxWidth: '300px', // Prevent it from being too huge
+          maxHeight: '400px', // Prevent it from being too tall
           borderRadius: '5px', 
           boxShadow: selected ? '0 0 0 2px #8e24aa' : 'none', 
         }} 
       />
 
       {/* --- CONNECTION DOTS --- */}
-      {/* Perfectly centered on edges using translate */}
-
-      {/* TOP */}
+      {/* Top */}
       <Handle 
         type="source" position={Position.Top} id="top" 
-        style={{ 
-          left: '50%', top: 0, transform: 'translate(-50%, -50%)',
-          width: 10, height: 10, background: '#555', border: '2px solid white', zIndex: 10 
-        }} 
+        style={{ left: '50%', top: 0, transform: 'translate(-50%, -50%)', width: 10, height: 10, background: '#555', border: '2px solid white', zIndex: 10 }} 
       />
-
-      {/* RIGHT */}
+      {/* Right */}
       <Handle 
         type="source" position={Position.Right} id="right" 
-        style={{ 
-          top: '50%', right: 0, transform: 'translate(50%, -50%)',
-          width: 10, height: 10, background: '#555', border: '2px solid white', zIndex: 10 
-        }} 
+        style={{ top: '50%', right: 0, transform: 'translate(50%, -50%)', width: 10, height: 10, background: '#555', border: '2px solid white', zIndex: 10 }} 
       />
-
-      {/* BOTTOM */}
+      {/* Bottom */}
       <Handle 
         type="source" position={Position.Bottom} id="bottom" 
-        style={{ 
-          left: '50%', bottom: 0, transform: 'translate(-50%, 50%)',
-          width: 10, height: 10, background: '#555', border: '2px solid white', zIndex: 10 
-        }} 
+        style={{ left: '50%', bottom: 0, transform: 'translate(-50%, 50%)', width: 10, height: 10, background: '#555', border: '2px solid white', zIndex: 10 }} 
       />
-
-      {/* LEFT */}
+      {/* Left */}
       <Handle 
         type="source" position={Position.Left} id="left" 
-        style={{ 
-          top: '50%', left: 0, transform: 'translate(-50%, -50%)',
-          width: 10, height: 10, background: '#555', border: '2px solid white', zIndex: 10 
-        }} 
+        style={{ top: '50%', left: 0, transform: 'translate(-50%, -50%)', width: 10, height: 10, background: '#555', border: '2px solid white', zIndex: 10 }} 
       />
     </div>
   );
